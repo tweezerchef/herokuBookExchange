3010c28ffe3926a0f3e100344b75ae0bdf4a7319
tweezerchef <ltomblock@gmail.com>
2023-11-18 16:39:32 -0600

    basicServer

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+
+  socket.on("sendMessage", (data) => {
+    io.emit("message", data); // Broadcast the message to all clients
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Socket.IO server listening on port ${PORT}`);
+});
