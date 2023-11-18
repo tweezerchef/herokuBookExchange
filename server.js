4fedec6c41b87c1020259b81b692650356843a17
tweezerchef <ltomblock@gmail.com>
2023-11-18 16:59:28 -0600

    changedBackPort

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Socket.IO server listening on port ${PORT}`);
 });
